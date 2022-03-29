baa8534f9cd60126141bd19f511c0337c356ed59
chungTA0609 <chungle.drive.tomari@gmail.com>
2022-03-29 11:56:17 +0700

    test

---
+++ first-app/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Form(props){
+    return(
+        <form>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input"></label>
+            </h2>
+        </form>
+    );
+}
