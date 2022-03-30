0a26c30c080b4bd7feb9562a62ebf4d7804fb368
chungTA0609 <chungle.drive.tomari@gmail.com>
2022-03-30 13:44:16 +0700

    test

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,45 +1,102 @@
 import logo from './logo.svg';
 import './App.css';
-import Todo from './components/Todo.js'
+import Todo from './components/Todo.js';
+import Form from './components/Form.js';
+import FilterButton from './components/FilterButton.js'
+import React, { useState, useRef, useEffect } from "react";
+import { nanoid } from "nanoid";
+
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
 
 
 function App(props) {
-  const taskList = props.tasks?.map(task => (<Todo name={task.name} completed={task.completed} id={task.id} key={task.id}/>));
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks?.length);
+
+  function toggleTaskCompleted(id) {
+    const updatedTask = tasks?.map(
+      task => {
+        if (id === task.id) {
+          return { ...task, completed: !task.completed }
+        }
+
+        return task
+      }
+    );
+    setTasks(updatedTask);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+
+  function editTasks(id, newName) {
+    const editedTaskList = tasks?.map(task => {
+      if (id === task.id) {
+        return { ...tasks, name: newName };
+      }
+      return tasks;
+    });
+    setTasks(editedTaskList);
+  }
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  const taskList = tasks?.filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTasks={editTasks}
+      />
+    ));
+
+  const tasksNoun = taskList?.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList?.length} ${tasksNoun} remaining`;
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input type="text" id="new-todo-input" className="input input__lg" name="text" autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        {filterList}
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
+      <h2 id="list-heading" tabIndex={"-1"} ref={listHeadingRef}>
+        {headingText}
       </h2>
       <ul role="list"
         className="todo-list stack-large stack-exception"
--- first-app/src/components/Form.js
+++ first-app/src/components/Form.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function Form(props){
+    function handleChange(e) {
+        setName(e.target.value);
+      }
+    const [name, setName] = useState('Use hooks!');
+    function handleChange(e) {
+        console.log("Typing!");
+      }
+      function handleSubmit(e){
+        e.preventDefault();
+        props.addTask(name);
+        setName("");
+    }
+    
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
-                <label htmlFor="new-todo-input"></label>
+                <label htmlFor="new-todo-input" className="label__lg">
+                    What needs to be done?
+                </label>
             </h2>
+            <input type="text" id="new-todo-input" className="input input__lg" name="text" autoComplete="off" value={name} onChange={handleChange}/>
+            <button type="submit" className="btn btn__primary btn__lg">
+                Add
+            </button>
         </form>
         
     );
 }
+
+
+
+export default Form;
